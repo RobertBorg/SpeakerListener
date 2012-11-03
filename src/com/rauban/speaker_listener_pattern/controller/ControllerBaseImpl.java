6f697b105c936d7d5c7dffab242f7d4bff345e83
Robert Borg <robert.borg@gmail.com>
2012-11-03 12:03:28 +0100

    Enforcing stricter typing to get the correct termology

--- src/com/rauban/speaker_listener_pattern/controller/ControllerBaseImpl.java
+++ src/com/rauban/speaker_listener_pattern/controller/ControllerBaseImpl.java
@@ -3,12 +3,12 @@ package com.rauban.speaker_listener_pattern.controller;
 import com.rauban.speaker_listener_pattern.model.ModelBaseImpl;
 import com.rauban.speaker_listener_pattern.speaker.Speaker;
 //these are bogous warnings that shouldn't be there -.- it won't accept ModelBaseImpl<? extends Speaker> 
-public class ControllerBaseImpl<T extends ModelBaseImpl> {
+public class ControllerBaseImpl<T extends ModelBaseImpl,V extends Speaker> {
 	protected T model;
 	public ControllerBaseImpl(T model){
 		this.model = model;
 	}
-	public  void addListener(Speaker listener){
+	public  void addListener(V listener){
 		model.addListener(listener);
 	}
 }
